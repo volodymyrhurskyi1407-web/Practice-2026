ca1e14747b9741631b41f9fdbdff6e92d8a1aed2
Volodymyr Hurskyi <volodymyr_hurskyi1407@tntu.edu.ua>
2026-09-08 12:53:40 +0300

    Видалено зайві системні папки

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_LibrarySystem_FILE C:/Users/Administrator/Practice-2026/build/LibrarySystem.exe)
-set(__QT_DEPLOY_TARGET_LibrarySystem_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_LibrarySystem_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
